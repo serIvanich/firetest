0c810421bda1d23a2cbbbf77624610890fece8d6
serIvanich <serivan1108@gmail.com>
2022-06-22 14:05:22 +0300

    added firebase+

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './App.module.css';
 import { AppRouter } from './components/AppRouter';
 import { Navbar } from './components/Navbar';
-import firebase from "firebase";
+
 
 function App() {
 
